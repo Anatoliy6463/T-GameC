656b901621324d6a0855dd79d5675bc9af2ed7b8
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-12-15 23:19:56 +0300

    Update game.c

--- game.c
+++ game.c
@@ -3,24 +3,34 @@
 Copyright (C) 2023  Andesaurus-Sesto*/
 #include "keys.h"
 #include "events.h"
-main()
+int main(int argc, char** argv)
 {
-  int in;
-  int X = 1;
-  int Y = 1;
-  int persiki;
-  int hp = 100;
-  int hunger = 0;
-  int ta = 0;
-  int money = 100;
-  int result;
-  while (in != 'q')
+  int X; int Y; int input; int persiki; int hp; int hunger; int ta; int money; int result;
+  input = getchar();
+  X = 1;
+  Y = 1;
+  hp = 100;
+  hunger = 0;
+  ta = 0;
+  money = 100;
+  while (input != 'q')
   {
-    in = getchar();
-    movement(X, Y, in);
-    persikinteract(X, Y, persiki, hunger, in);
-    heal(hunger, hp);
-    damage(hunger, hp);
+    if (input == 'w' || input == 'a' || input == 's' || input == 'd')
+    {
+      movement(X, Y, input);
+    }
+    if (input == 'p' || input == 'e')
+    {
+      persikinteract(X, Y, persiki, hunger, input);
+    }
+    if (hunger < 0)
+    {
+      heal(hunger, hp);
+    }
+    if (hunger > 100)
+    {
+      damage(hunger, hp);
+    }
     if (X % 45 == 0 && Y % 10 == 0)
     {
       shop(money, ta, persiki);
@@ -29,7 +39,7 @@ main()
     {
       teleport(X, Y, result, money);
     }
-    if (in == 't')
+    if (input == 't')
     {
       check(ta, result);
       if (result == 1)
@@ -38,14 +48,17 @@ main()
       }
     }
     hunger++;
-    if (hp == ']')
+    if (input == ']')
     {
       die();
+      break;
     }
     if (hp == 0)
     {
       die();
+      break;
     }
-    printf("Координаты:%d,%d\nЗдоровье:%d\nУровень голода:%d\nУ вас %d либрбаксов\n");
+    printf("Координаты:X=%d Y=%d\nЗдоровье:%d\nУровень голода:%d\nУ вас %d либрбаксов\n", X, Y, hp, hunger, money);
+    input = getchar();
   }
 }
