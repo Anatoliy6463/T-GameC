2ef63c87133810850dd9b2656482191f25791220
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-12-15 23:20:52 +0300

    Update keys.h

--- keys.h
+++ keys.h
@@ -1,24 +1,26 @@
 #include <stdio.h>
 /*Library for controls in T-GameC
 Copyright (C) 2023  Andesaurus-Sesto*/
-void movement(int X, int Y, int inp)
+void movement(int X, int Y, int in)
 {
-    if (inp == 'w') ++Y;
-    if (inp == 'a') --X;
-    if (inp == 's') --Y;
-    if (inp == 'd') ++X;
+  in = getchar();
+  if (in == 'w') Y++;
+  if (in == 'a') X++;
+  if (in == 's') Y++;
+  if (in == 'd') X++;
+  return;
 }
-void persikinteract(int X, int Y, int persiki, int hunger, int inp)
+void persikinteract(int X, int Y, int persiki, int hunger, int in)
 {
     if (X % 9 == 0 && X != 0 || Y % 9 == 0 && Y != 0) ++persiki;  
-    if (inp == 'p') printf("Количество персиков:%d\n", persiki);
-    if (inp == 'e' && persiki > 0) 
+    if (in == 'p') printf("Количество персиков:%d\n", persiki);
+    if (in == 'e' && persiki > 0) 
     {
 	puts("Вы съели персик!\n");
 	if (hunger <= 20) hunger = 0;
         if (hunger > 20) hunger = hunger - 20;
-	--persiki;
+	persiki++;
     }
-    if (inp == 'e' && persiki == 0)
+    if (in == 'e' && persiki == 0)
       puts("У вас нет персиков, найдите или купите их.\n");
 }
